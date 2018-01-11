305fd08f7434dc5c5e41629b2e459df70ea0443a
merlox <merloxdixcontrol@gmail.com>
2018-01-11 15:11:01 +0100

    Updated webBrowser with commander instead of a manual solution. Now you need to specify the url with the --url flag

--- apps/webBrowser.js
+++ apps/webBrowser.js
@@ -1,15 +1,20 @@
 const request = require('request-promise')
 const fs = require('fs')
-// Take the latest parameter as the url
-const url = process.argv[process.argv.length - 1]
+const commander = require('commander')
+
+commander
+	.option('-u, --url <url>', 'The url you want to get [url]', 'https://google.com')
+	.parse(process.argv)
+
+if(!commander.url) commander.url = 'https://google.com'
+
+init()
 
 async function init() {
 	try {
-		const page = await request(url)
+		const page = await request(commander.url)
 		fs.writeFileSync('page.html', page)
 	} catch (e) {
 		console.log('Error when processing the request', e)
 	}
 }
-
-init()
