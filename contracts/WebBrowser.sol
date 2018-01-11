3373b1c1a379aaebde50241d7e13a7c9953cf187
merlox <merloxdixcontrol@gmail.com>
2018-01-11 12:49:07 +0100

    Modified dapp price to 1 RLC

--- contracts/WebBrowser.sol
+++ contracts/WebBrowser.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.11;
 import "iexec-oracle-contract/contracts/IexecOracleAPI.sol";
 contract WebBrowser is IexecOracleAPI {
 
-    uint public constant DAPP_PRICE = 0;
+    uint public constant DAPP_PRICE = 1;
     string public constant DAPP_NAME = "webbrowser";
 
     function WebBrowser (address _iexecOracleAddress) IexecOracleAPI(_iexecOracleAddress,DAPP_PRICE,DAPP_NAME){
